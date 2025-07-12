25b8324077b0678f4b68109b26b12e695e5b4f86
ChenLin Wang <wangchenlin2001@icloud.com>
2025-07-12 15:01:54 +0800

    V1.2.4.37

--- ci-testing/Tests/ci-testingTests/ci_testingTests.swift
+++ ci-testing/Tests/ci-testingTests/ci_testingTests.swift
@@ -3,7 +3,11 @@ import Foundation
 @testable import ci_testing
 
 @Test func connectToPostgres() async throws {
+    #if os(macOS)
+    let res = try Sh.run("/opt/homebrew/opt/postgresql@17/bin/psql -h localhost -p 5432 -d testing -U testing", paras: ["PGPASSWORD" : "123456"])
+    #else
     let res = try Sh.run("psql -h localhost -p 5432 -d testing -U testing", paras: ["PGPASSWORD" : "123456"])
+    #endif
     
     if let res = String(data: res.res, encoding: .utf8) {
         print(res)
@@ -13,7 +17,11 @@ import Foundation
 }
 
 @Test func connectToPostgres2() async throws {
+    #if os(macOS)
+    let res = try Sh.run("/opt/homebrew/opt/postgresql@17/bin/psql -h localhost -p 5432 -d testing2 -U testing", paras: ["PGPASSWORD" : "pass1"])
+    #else
     let res = try Sh.run("psql -h localhost -p 5432 -d testing2 -U testing", paras: ["PGPASSWORD" : "pass1"])
+    #endif
     
     if let res = String(data: res.res, encoding: .utf8) {
         print(res)
@@ -23,7 +31,11 @@ import Foundation
 }
 
 @Test func connectToPostgres3() async throws {
+    #if os(macOS)
+    let res = try Sh.run("/opt/homebrew/opt/postgresql@17/bin/psql -h localhost -p 5433 -d db1 -U user1", paras: ["PGPASSWORD" : "pass1"])
+    #else
     let res = try Sh.run("psql -h localhost -p 5433 -d db1 -U user1", paras: ["PGPASSWORD" : "pass1"])
+    #endif
     
     if let res = String(data: res.res, encoding: .utf8) {
         print(res)
