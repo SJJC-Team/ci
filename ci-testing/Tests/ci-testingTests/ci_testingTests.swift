5cfe8b8bc3f4479f9ab4513de9fb1b72fed0d588
ChenLin Wang <wangchenlin2001@icloud.com>
2026-02-05 12:02:14 +0800

    V1.2.6.7

--- ci-testing/Tests/ci-testingTests/ci_testingTests.swift
+++ ci-testing/Tests/ci-testingTests/ci_testingTests.swift
@@ -48,7 +48,7 @@ import Foundation
     #if os(macOS)
     let res = try Sh.run("lsof -i:8282")
     #else
-    let res = try Sh.run("netstat -tlnp | grep :8282")
+    let res = try Sh.run("ss -ant | grep 8282")
     #endif
     
     if let res = String(data: res.res, encoding: .utf8) {
