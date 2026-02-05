9a7c6a2f5914dd8bffb8c200b88626ec62c4ea7a
ChenLin Wang <wangchenlin2001@icloud.com>
2026-02-05 12:18:23 +0800

    V1.2.6.10 增加系统初始化步骤，安装缺失的工具

--- .github/workflows/ubuntu/postgres.sh
+++ .github/workflows/ubuntu/postgres.sh
@@ -15,8 +15,6 @@ usermod -aG sudo pg_tester
 
 echo installing postgresql@${1}
 
-apt-get update
-apt-get install curl ca-certificates -y
 install -d /usr/share/postgresql-common/pgdg
 curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
 . /etc/os-release
